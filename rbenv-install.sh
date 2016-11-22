057755e131ee4a8c339bedb9d984d43d8b9c39ee
pzolta <pzoltan7@gmail.com>
2016-11-22 19:27:21 +0100

    Added rbenv-install.sh

---
+++ rbenv-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd
+git clone git://github.com/sstephenson/rbenv.git .rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
+source ~/.bash_profile
