6d12ade6a7654f9dd7fea401fd00be898af44b1c
pzolta <pzoltan7@gmail.com>
2016-11-22 20:28:28 +0100

    Further bugfix to installer

--- rbenv-install.sh
+++ rbenv-install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd
 git clone git://github.com/sstephenson/rbenv.git .rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'export PATH="/root/.rbenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
+echo 'export PATH="/root/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
 source ~/.bash_profile
 rbenv install -v 2.2.3
 rbenv global 2.2.3
