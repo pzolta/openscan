c34a4bf4790b7cae16c2c4429ab31264eda01c3f
pzolta <pzoltan7@gmail.com>
2016-12-02 16:08:58 +0100

    Add files via upload

---
+++ openvas-config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+openvas-nvt-sync
+openvas-scapdata-sync
+openvas-certdata-sync
+service openvas-scanner restart
+service openvas-manager restart
+service openvasmd --rebuild --progress
+openvasmd-stop
+openvasmd --create-user=vulscan --role=Admin
+openvasmd --user=vulscan --new-password=vulscan
+openvasmd-start
